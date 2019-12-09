682efa32c6212c1b084ae1986ca815f256cf23ba
Srikar Valluri <srikarvalluri@Srikars-MacBook-Pro.local>
2019-12-08 22:58:50 -0800

    Added final stuff

--- motor_hbridge_user_input.c
+++ motor_hbridge_user_input.c
@@ -1,4 +1,3 @@
-
 // Analog Pins for Motor 
 int enA = 9;
 int in1 = 8;
@@ -9,10 +8,10 @@ int motor_speed = 0;
 int potentiometer_analog_input = A0;  
 
 struct data{
-    float potentiometer_analog_input;
+    float user_input;
     int motor_speed;
 };
-struct data data;
+struct data* data;
 
 void setup(){
   pinMode(enA, OUTPUT);
@@ -26,15 +25,16 @@ void loop(){
   digitalWrite(in2, LOW);
   
   // Read the values from the potentiometers
-  data.potentiometer_analog_input = analogRead(potentiometer_analog_input); 
+  while(Serial.available() == 0) {}
+  data->user_input = Serial.parseInt(); // read user input
   
   // Use the map function to automatically scale analog values from a potentiometer scale to a motor scale
-  data.motor_speed = map(data.potentiometer_analog_input, 0, 1023, 0, 255);
+  data->motor_speed = data->user_input;
   
   // Adjust to prevent "buzzing" at very low speed
-  if(data.motor_speed < 8){
-    data.motor_speed = 0;
+  if(data->motor_speed < 8){
+    data->motor_speed = 0;
   }
   // Set the motor speeds
-  analogWrite(enA, data.motor_speed);
+  analogWrite(enA, data->motor_speed);
 }
